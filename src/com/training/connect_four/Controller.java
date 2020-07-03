16d75144601e86851ceb34a5e51a70a94c56133a
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-03 19:38:04 +0530

    First commit

---
+++ src/com/training/connect_four/Controller.java
@@ -0,0 +1,29 @@
+package com.training.connect_four;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	@FXML
+	public GridPane rootGridPane;
+
+	@FXML
+	public Pane insertedDiscsPane;
+
+	@FXML
+	public Label playerNameLabel;
+
+
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+	}
+}
